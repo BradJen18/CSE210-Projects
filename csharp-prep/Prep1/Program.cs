110108dbf2aa6281deb362c0d4698c754438b849
BradJen18 <jen22041@byui.edu>
2026-01-10 17:15:49 -0700

    Prep 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first name? ");
+        string First = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string Last = Console.ReadLine();
+
+        Console.Write("");
+        Console.WriteLine($"Your name is {Last}, {First} {Last}.");
     }
 }
+
