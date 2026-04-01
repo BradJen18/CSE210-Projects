3c9f7ce62e8dae382b579e736608a701266ac602
BradJen18 <jen22041@byui.edu>
2026-04-01 00:32:03 -0600

    Develop 03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,23 +1,33 @@
 using System;
 using System.Collections.Concurrent;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the reference?");
-        string input1 = Console.ReadLine();
+
+        Scripture scripture = new Scripture();
+
+        Console.Clear();
+        scripture.SetReference1("1 Nephi",11,17); // Set scripture Reference (if more than 1 verse switch "SetReference1" to "SetReference2")
+        scripture.SetWords("And I said unto him: I know that he loveth his children; nevertheless, I do not know the meaning of all things."); // Set Scripture
+        scripture.RenderAll();
+        Console.WriteLine();
+        Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+        string input = Console.ReadLine();
 
 
-
-        Console.WriteLine("What is the scripture?");
-        string input2 = Console.ReadLine();
-
-
-        // Scripture scripture = new Scripture();
-
-        // scripture.HideWords();
-
-
+        int num = scripture.CompleteHide();
+        while (num > 0 && input != "quit")
+        {
+            Console.Clear();
+            scripture.HideWords();
+            scripture.RenderAll();
+            input = Console.ReadLine();
+            num = scripture.CompleteHide();
+            Console.Clear();
+        }
+        Console.Clear();
     }
 }
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -5,35 +5,102 @@ public class Scripture
     private List<Word> _word = new List<Word>();
     private Reference _reference = new Reference();
 
-    
-
     public void HideWords()
-    {
+    {   
+        Random random = new Random();
+            foreach (Word w in _word)
+            {
+                int hide = random.Next(2);
+                if (hide == 1)
+                {
+                    w.SetHidden(false);
+                }
+            }
         
+    }
+    public int CompleteHide()
+    {
         int count = 0;
-        foreach (Word w in _word) // Count all words in the scripture, use count to randomly pick a word in the list. when random word is picked, change "hidden" to a 1 by doing "hidden = 1"
+        foreach (Word w in _word)
         {
             count += 1;
         }
-        Console.WriteLine(count);
-
-        Random random = new Random();
-        int number = random.Next(count); // need to find out how to single out a word in a list & chaince the value.
-
+        int num = count;
+            foreach (Word w in _word)
+            {
+                if (w.GetHidden() == false)
+                {
+                    num = num - 1;
+                }
+            }
+        return num;
     }
     public void RenderText()
     {
-        
+        foreach (Word w in _word)
+        {
+            if (w.GetHidden() == true)
+            {
+                Console.Write($"{w.GetWord()} ");
+            }
+            else
+            {
+                Console.Write("_ ");
+            }
+        }
     }
     public void RenderReference()
     {
-        string book = _reference.GetBook();
-        int chapter = _reference.GetChapter();
-        int verse = _reference.GetVerse();
-        int endVerse = _reference.GetEndVerse();
+        if (_reference.GetEndVerse() != 0)
+        {
+            string book = _reference.GetBook();
+            int chapter = _reference.GetChapter();
+            int verse = _reference.GetVerse();
+            int endVerse = _reference.GetEndVerse();
 
-        Console.WriteLine($"{book} {chapter}:{verse} {endVerse}");
+            Console.WriteLine($"{book} {chapter}:{verse}-{endVerse}");
+        }
+        else
+        {
+            string book = _reference.GetBook();
+            int chapter = _reference.GetChapter();
+            int verse = _reference.GetVerse();
+            Console.WriteLine($"{book} {chapter}:{verse}");
+
+        }
 
     }
+
+    public void RenderAll()
+    {
+        RenderReference();
+        RenderText();
+    }
+
+    public void SetReference1(string book, int chapter, int verse)
+    {
+        _reference.SetBook(book);
+        _reference.SetChapter(chapter);
+        _reference.SetVerse(verse);
+    }
+    public void SetReference2(string book, int chapter, int verse, int endVerse)
+    {
+        _reference.SetBook(book);
+        _reference.SetChapter(chapter);
+        _reference.SetVerse(verse);
+        _reference.SetEndVerse(endVerse);
+    }
+    public void SetWords(string scripture)
+    {   
+        string phrase = scripture;
+        string separator = " ";
+        string [] words = phrase.Split(separator);
+        foreach (string w in words)
+        {
+            Word word = new Word(w, true);
+            _word.Add(word);
+        }
+    }
+    
 }
 
